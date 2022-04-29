ca34fa74cb01423e2111320bf703dcfcb55ee639
Arhontes <arhontes@yandex.ru>
2022-04-29 11:59:47 +0300

    Done first homework from Ignat. Used flex in css.

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import message from './Message.module.css'
+
+export type messagePropsType = {
+    avatar: string,
+    name: string,
+    message: string,
+    time: string
+}
+
+export const Message = (props: messagePropsType) => {
+    return (
+        <div className={message.message_container}>
+
+            <img className={message.profile_ico} src={props.avatar} alt=""/>
+
+            <div className={message.flex_container}>
+                <div className={message.container}>
+                    <div className={message.appendix}>
+
+                    </div>
+                </div>
+
+                <div className={message.flex_box}>
+                    <div className={message.wrapper}>
+                        <div className={message.dialog}>
+                            <div className={message.dialog_wrapper}>
+                                <div className={message.name}>
+                                    {props.name}
+                                </div>
+                                <div className={message.message_text}>
+                                    {props.message}
+                                </div>
+                            </div>
+
+                        </div>
+                        <div className={message.time_info}>
+                            {props.time}
+                        </div>
+
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    )
+}
+

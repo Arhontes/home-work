adeb4f73b9e92e0f0cddf6fcff6895212bb8845f
Alex Betke <arhontes@yandex.ru>
2022-05-29 16:11:15 +0300

    Added css module to navigate

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+import {PATH} from "./Pages";
+import s from '../../NavLink.module.css'
+
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
+        <div className={s.menu}>
 
+                <div className={s.label}>Level</div>
+                <span className={s.item}><NavLink  to={PATH.PRE_JUNIOR}>Pre-Junior</NavLink></span>
+                <span className={s.item}><NavLink to={PATH.JUNIOR}>Junior</NavLink></span>
+                <span className={s.item}><NavLink  to={PATH["JUNIOR+"]}>Junior+</NavLink></span>
+                {/*<div className="item"><span>Behance</span></div>
+                <div className="item"><span>MixCloud</span></div>
+                <div><NavLink  to={PATH.PRE_JUNIOR}>Pre-Junior</NavLink></div>
+                <div><NavLink to={PATH.JUNIOR}>Junior</NavLink></div>
+                <div><NavLink  to={PATH["JUNIOR+"]}>Junior+</NavLink></div>*/}
         </div>
     )
 }
-
+{/*className={navData => navData.isActive ? n.active : n.item}*/}
 export default Header
